cec28489f198bbee0f9c529c0ed3c8edc9d4717e
DMelnev <59581747+DMelnev@users.noreply.github.com>
2022-04-23 23:53:35 +0300

    created test quake

---
+++ src/test/java/Lesson3/QuakeTest.java
@@ -0,0 +1,21 @@
+package Lesson3;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class QuakeTest {
+    @Before
+    void before(){
+
+    }
+    @BeforeEach
+    void setUp(){
+
+    }
+
+    @Test
+    public  void authorisation(){
+
+    }
+}
