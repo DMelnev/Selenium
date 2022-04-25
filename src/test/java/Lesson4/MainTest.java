42525d5841509cbe38c08e1733dd929313affe15
DMITRIY <dvmau@mail.ru>
2022-04-25 15:20:31 +0300

    add junit5 test

--- src/test/java/Lesson4/MainTest.java
+++ src/test/java/Lesson4/MainTest.java
@@ -1,17 +1,12 @@
 package Lesson4;
 
-import org.junit.Assert;
-import org.junit.Test;
 
-public class MainTest {
+import org.junit.jupiter.api.Test;
+
+class MainTest {
 
     @Test
-    public void rectangleSquare() {
-        Assert.assertEquals(0, Main.rectangleSquare(0, 4, 5), 0.001);
-        Assert.assertEquals(0, Main.rectangleSquare(3, 0, 5), 0.001);
-        Assert.assertEquals(0, Main.rectangleSquare(3, 4, 0), 0.001);
-        Assert.assertEquals(0, Main.rectangleSquare(-1, 4, 5), 0.001);
-        Assert.assertEquals(6, Main.rectangleSquare(3, 4, 5), 0.001);
-        Assert.assertEquals(600000000, Main.rectangleSquare(30000,40000, 50000), 0.001);
+
+    void rectangleSquare() {
     }
 }
